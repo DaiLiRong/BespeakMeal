55c194a8b39d3ab2f0f90270acd3b0aebe5038ca
lirong dai <xhyzdai@qq.com>
2012-05-24 22:33:28 +0800

    commit_1

---
+++ BespeakMeal/BespeakMeal.Domain/Entities/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BespeakMeal.Domain.Entities
+{
+	/// <summary>
+	/// 编写持久化类,来映射成为数据库表
+	/// </summary>
+	public class User
+	{
+		public virtual int UserId { get; set; }
+		public virtual string UserName { get; set; }
+		public virtual string Password { get; set; }
+		public virtual string FirstName { get; set; }
+		public virtual string LastName { get; set; }
+		public virtual char Gender { get; set; }
+		public virtual string PhoneNum { get; set; }
+		public virtual string Email { get; set; }
+		public virtual int CreateTime { get; set; }
+		public virtual int SuperUser { get; set; }
+		public virtual int Status { get; set; }
+	}
+}
---
+++ BespeakMeal/BespeakMealTest/BespeakMeal.Test.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using NHibernate;
+using NHibernate.Cfg;
+using BespeakMeal.Data;
+using BespeakMeal.Domain.Entities;
+namespace BespeakMeal.Test
+{
+	[TestClass]
+	public class BespeakMealTest
+	{
+		private UserData _user;
+		private ISession _session;
+
+		#region 附加测试特性
+		//
+		// 编写测试时，可以使用以下附加特性:
+		//
+		// 在运行类中的第一个测试之前使用 ClassInitialize 运行代码
+		// [ClassInitialize()]
+		// public static void MyClassInitialize(TestContext testContext) { }
+		//
+		// 在类中的所有测试都已运行之后使用 ClassCleanup 运行代码
+		// [ClassCleanup()]
+		// public static void MyClassCleanup() { }
+		//
+		// 在运行每个测试之前，使用 TestInitialize 来运行代码
+		// [TestInitialize()]
+		// public void MyTestInitialize() { }
+		//
+		// 在每个测试运行完之后，使用 TestCleanup 来运行代码
+		// [TestCleanup()]
+		// public void MyTestCleanup() { }
+		//
+		#endregion
+
+		[TestMethod]
+		public void TestUser()
+		{
+			/*var tempUser = new User { UserName = "lion", Password = "123", 
+				FirstName = "戴", LastName = "礼荣", Gender = '男', PhoneNum = "12345678901", Email = "xhyzdai@qq.com" };*/
+			_session = new NHibernateHelper().GetSession();
+			_user = new UserData(_session);
+			//_user.CreateUser(tempUser);
+			User userInfo = _user.GetUserById(1);
+			string userName = userInfo.FirstName;
+			Assert.AreEqual("礼荣", userName);
+		}
+	}
+}
